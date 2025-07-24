40a407985e82a20d87b223fe21fd6f7d3e0c3be5
lovefordino <lovefordino211@gmail.com>
2025-07-24 18:14:40 +0900

    테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import AdminPage from './pages/AdminPage';
 import DrawPage from './pages/DrawPage';
+import AdminPage from './pages/AdminPage';
 import AdminLoginPage from './pages/AdminLoginPage';
-import useAuthStore from './store/useAuthStore';
 
 function App() {
-  const isAdmin = useAuthStore((s) => s.isAdmin);
-
   return (
     <Router>
       <Routes>
         <Route path="/" element={<DrawPage />} />
         <Route path="/admin" element={<AdminPage />} />
+        <Route path="/admin-login" element={<AdminLoginPage />} />
       </Routes>
     </Router>
   );
